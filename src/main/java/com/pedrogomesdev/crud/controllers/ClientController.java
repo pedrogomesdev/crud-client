d857fd44c3ed7e5faea9c5d3b832cabfc41da933
Pedro Gomes <pgestudos2022@gmail.com>
2023-09-29 18:31:03 -0300

    Busca paginada

--- src/main/java/com/pedrogomesdev/crud/controllers/ClientController.java
+++ src/main/java/com/pedrogomesdev/crud/controllers/ClientController.java
@@ -1,6 +1,8 @@
 package com.pedrogomesdev.crud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,4 +26,9 @@ public class ClientController {
 		return ResponseEntity.ok(dto);
 	}
 	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+		Page<ClientDTO> dto = service.findAll(pageable);
+		return ResponseEntity.ok(dto);
+	}
 }
--- src/main/java/com/pedrogomesdev/crud/services/ClientService.java
+++ src/main/java/com/pedrogomesdev/crud/services/ClientService.java
@@ -1,6 +1,8 @@
 package com.pedrogomesdev.crud.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,12 @@ public class ClientService {
 	public ClientDTO findId(Long id) { 
 		Client client = repository.findById(id).orElseThrow(() -> new NotFoundException("Recurso não encontrado")) ;
 		return new ClientDTO(client);		
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable page) { 
+		Page<Client> client = repository.findAll(page);
+		return client.map(x -> new ClientDTO(x));		
 	}
 	
 }
